2bb2333be9729a38348ce1c09859d90799b08367
zartul <121471819+zartul@users.noreply.github.com>
2025-02-11 12:15:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //complete this code
 class Person {
 	constructor(name,age){
-		this.name=name;
-		this.age=age;
+		this._name=name;
+		this._age=age;
 	}
 	get Name(){
-		return this.name;
+		return this._name;
 	}
-	set Age(newAge){
-		this.age=newAge;
+	set Age(age){
+		this._age=age;
 	}
 }
 
