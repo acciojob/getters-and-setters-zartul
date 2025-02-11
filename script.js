69ba5d5e9bdccbd84d4adefc433de108c2b1aa54
zartul <121471819+zartul@users.noreply.github.com>
2025-02-11 12:08:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+	get Name(){
+		return this.name;
+	}
+	set Age(){
+		return this.age;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(`${this.name} is studying`);
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name} is studying` )
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
