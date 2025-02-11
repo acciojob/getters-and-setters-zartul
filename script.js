e849d30ed702db221eecf25018a54c007c01ed44
zartul <121471819+zartul@users.noreply.github.com>
2025-02-11 12:20:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ class Person {
 
 class Student extends Person {
 	study(){
-		console.log(`${this.name} is studying`);
+		console.log(`${this._name} is studying`);
 	}
 }
 
 class Teacher extends Person {
 	teach(){
-		console.log(`${this.name} is studying` )
+		console.log(`${this._name} is studying` )
 	}
 }
 
